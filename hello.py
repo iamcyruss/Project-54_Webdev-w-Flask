c169f65d3663be9fe3dae2b7c564baaece7be1a2
iamcyruss <russnicosia@gmail.com>
2023-02-08 02:58:29 -0700

    initial commit

--- hello.py
+++ hello.py
@@ -9,6 +9,9 @@ then need to set the environment variable
 $env:FLASK_APP="hello.py"
 then run the server
 flask run
+to get venv to run properly I needed to change something for windows
+https://stackoverflow.com/questions/54776324/powershell-bug-execution-of-scripts-is-disabled-on-this-system 
+I think I did it just for my user. still risky though...
 """
 
 
